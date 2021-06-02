999a9fbdb0715e4ffdc1691c8e08827be436ad42
Katakurinna <alesagustin@gmail.com>
2021-06-02 17:30:32 +0200

    DRAFT

--- src/main/java/me/cerratolabs/rust/servermanager/config/RustConfig.java
+++ src/main/java/me/cerratolabs/rust/servermanager/config/RustConfig.java
@@ -1,17 +1,15 @@
 package me.cerratolabs.rust.servermanager.config;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
+@Data
 @Configuration
 @ConfigurationProperties(prefix = "rust")
-@Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@PropertySource("classpath:application.properties")
 public class RustConfig {
     private String secretKey;
     private String steamKey;
